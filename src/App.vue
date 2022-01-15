29a5dd28d886c41e7bb3f5b8592ccbb6222c2bd4
Shivji999 <71450285+Shivji999@users.noreply.github.com>
2022-01-15 10:35:09 +0530

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <section>
+    <header>
+      <h1>My Friends</h1>
+    </header>
+    <ul>
+      <friend-contact
+        name="shivam sharma"
+        phone-number="0123 4567 890"
+        email-address="shiva@gmail.com "
+        hobby="playing"
+      >
+      </friend-contact>
+      <friend-contact
+        name="sanjana sharma"
+        phone-number="0433 4537 840"
+        email-address="sanjana@gmail.com"
+        hobby="fighting"
+      >
+      </friend-contact>
+    </ul>
+  </section>
+</template>
+
+<script>
+import FriendContact from "./components/FriendContact.vue";
+export default {
+  components: { FriendContact },
+  data() {
+    return {
+      friends: [
+        // {
+        //   id: "shiv",
+        //   name: "shiv sharma",
+        //   phone: "0123  4567 890",
+        //   email: "shiva@gmail.com",
+        // },
+        // {
+        //   id: "sanjana",
+        //   name: "sanjana sharma",
+        //   phone: "0433  4537 840",
+        //   email: "sanjana@gmail.com",
+        // },
+      ],
+    };
+  },
+};
+</script>
